fb15ec69b02ccac7a8161339f6ce173ad195ed98
xpy <pavloschris@gmail.com>
2016-10-18 21:58:47 +0300

    76

---
+++ 76.py
@@ -0,0 +1,29 @@
+import time
+import helpers
+import math
+
+# inputFile = open('resources/', 'r')
+
+start = time.clock()
+
+cache = {}
+
+
+def do_it(num, limit):
+    cache_key = (num, limit)
+    c = cache.get(cache_key)
+    if c:
+        return c
+    s = 0
+    for i in range(limit, 0, -1):
+        n2 = num - i
+        s += 1 if n2 <= 1 else do_it(n2, min(i, n2))
+    cache[cache_key] = s
+    return s
+
+
+print(do_it(100, 99))
+# print(cache)
+# 190569291
+
+print(time.clock() - start)
