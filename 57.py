f1ccb713e8cf5208b192d8100a290c1604976478
xpy <pavloschris@gmail.com>
2016-11-17 19:45:03 +0200

    57

--- 57.py
+++ 57.py
@@ -2,24 +2,24 @@ import time
 import helpers
 
 start = time.clock()
-k= 0
+k = 0
 numerator = 3
 denominator = 2
 prevNumerator = 1
 prevDenominator = 1
 results = []
 while k < 1000:
-    k+=1
-    if(len(str(numerator)) > len(str(denominator))):
-        results.append({'numerator':numerator,'denominator':denominator})
+    k += 1
+    if len(str(numerator)) > len(str(denominator)):
+        results.append({'numerator': numerator, 'denominator': denominator})
     tmpNumerator = numerator
     tmpDenominator = denominator
-    numerator = numerator*2 + prevNumerator
-    denominator = denominator*2 + prevDenominator
+    numerator = numerator * 2 + prevNumerator
+    denominator = denominator * 2 + prevDenominator
     prevNumerator = tmpNumerator
     prevDenominator = tmpDenominator
 
-print len(results)
+print(len(results))
 
-print '-' * 20
-print time.clock() - start
+print('-' * 20)
+print(time.clock() - start)
