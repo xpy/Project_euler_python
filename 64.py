c20aa7ab7e87353fb081b94a9b57793c035b313c
xpy <pavloschris@gmail.com>
2017-08-11 01:28:31 +0300

    64

--- 64.py
+++ 64.py
@@ -4,32 +4,41 @@ import math
 
 start = time.clock()
 
-sequence = []
 num_of_sequences = 0
 
 
 def fraction_to(num, to=0, a=None, m=0, d=1):
-    global num_of_sequences
-    a = int(math.sqrt(num)) if not a else a
+    global num_of_sequences, sequence_length, truples
+    a = a if a else int(math.sqrt(num))
+    truple = (a, m, d)
     next_m = d * a - m
     next_d = int((num - next_m ** 2) / d)
     next_a = int((int(math.sqrt(num)) + next_m) / next_d)
-    # print(next_a, next_m, next_d, )
-    sequence.append(next_a)
-    if len(sequence)-1 % 2 == 0 and len(sequence) > 1:
-        print(sequence)
-        if sequence[1:len(sequence)-1 // 2] == sequence[len(sequence)-1 // 2:]:
-            if len(sequence) // 2 % 2:
-                num_of_sequences += 1
-                print('AAAAAAAAAA')
-            return
+    if truple in truples:
+        num_of_sequences += sequence_length % 2
+        # if sequence_length % 2 == 0:
+        #     # print('LOOOOOP', num)
+        #     # print(num, sequence)
+        #     # print('-' * 5)
+        #     num_of_sequences += 1
+        return
+    truples.add(truple)
+    # sequence.append(next_a)
+    sequence_length += 1
     if to > 0:
         fraction_to(num, to - 1, next_a, next_m, next_d)
 
 
-for i in range(2, 14):
-    if i ** .5 % 2:
-        fraction_to(i, 100)
+# params = set()
+# sequence = []
+# fraction_to(23, 100)
+
+for i in range(2, 10000):
+    if i ** .5 != int(i ** .5):
+        truples = set()
+        sequence = []
+        sequence_length = 1
+        fraction_to(i, 20000)
 
 print(num_of_sequences)
 print(time.clock() - start)
