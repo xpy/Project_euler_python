bd8b0bab3fa8c72243e4724d36c914ead535c8c3
xpy <pavloschris@gmail.com>
2017-08-29 18:24:10 +0200

    78 and 78a and 100 wip

--- 100.py
+++ 100.py
@@ -1,6 +1,9 @@
 import time
 import helpers
 import math
+from decimal import *
+
+getcontext().prec = 100
 
 # inputFile = open('resources/', 'r')
 
@@ -10,7 +13,7 @@ start = time.clock()
 def f(a):
     # d = math.sqrt(-4 * a * a + -4 * a + 4)
     b = (2 - 4 * a)
-    d = math.sqrt(b ** 2 - 4 * (a ** 2 - a) * 2)
+    d = Decimal(b ** 2 - 4 * (a ** 2 - a) * 2).sqrt()
     # print('b', b)
     # print('din', b ** 2 - 4 * (a ** 2 - a) * 2)
     # print('d', d)
@@ -19,7 +22,9 @@ def f(a):
 
 for i in range(10 ** 12, 10 ** 13):
     res = f(i)
-    if res[0].is_integer():
+    if i % 10000 == 0:
+        print(i)
+    if res[0] % 1 == 0:
         sadf = ((i - res[0]) / i) * ((i - res[0] - 1) / (i - 1))
         print(i, res, sadf)
         print(i - res[0], i - res[1])
