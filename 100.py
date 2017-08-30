c74fa52dd9d429fae8714d9e1b13b6bcfc165054
xpy <pavloschris@gmail.com>
2017-08-30 17:10:57 +0200

    78 and 78a and 100 wip

--- 100.py
+++ 100.py
@@ -12,19 +12,26 @@ start = time.clock()
 
 def f(a):
     # d = math.sqrt(-4 * a * a + -4 * a + 4)
+    # d = b ** 2 - 4 * (a ** 2 - a) * 2
+    d = 4 - 8 * a*(1-a)
+    #   4 -b**2 + 8 * a**2 -8*a =0
+    # print(int(d**.5),int(d**.5)**2,d)
+    if int(d ** .5) ** 2 != d:
+        return False
+    print('?')
     b = (2 - 4 * a)
-    d = Decimal(b ** 2 - 4 * (a ** 2 - a) * 2).sqrt()
+    sqrt_d = Decimal(d).sqrt()
     # print('b', b)
     # print('din', b ** 2 - 4 * (a ** 2 - a) * 2)
     # print('d', d)
-    return (b * -1 - d) / 4, (b * -1 + d) / 4
+    return (b * -1 - sqrt_d) / 4, (b * -1 + sqrt_d) / 4
 
 
-for i in range(10 ** 12, 10 ** 13):
+for i in range(10**1, 10 ** 13):
     res = f(i)
-    if i % 10000 == 0:
+    if i % 1000000 == 0:
         print(i)
-    if res[0] % 1 == 0:
+    if res and res[0] % 1 == 0:
         sadf = ((i - res[0]) / i) * ((i - res[0] - 1) / (i - 1))
         print(i, res, sadf)
         print(i - res[0], i - res[1])
